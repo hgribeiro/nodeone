676d38fe9cd86b97fff95c70b4cb0acf37f39c35
Hugo Ribeiro <hgribeirolive@gmail.com>
2020-04-01 12:07:41 -0300

    obj on print

---
+++ index.js
@@ -0,0 +1,15 @@
+// This is a obj that has other obj as atribute
+const empresa = { 
+  nome: "RocketSeat",
+  cor: "Roxo",
+  foco: "programacao",
+  endereco: {
+    rua: "rua tal tal tal",
+    numero: 123
+  }
+};
+
+// Now I will print this obj on the cosole
+
+console.log (`O nome da empresa é ${empresa.nome}, a cor é ${empresa.cor}!`);
+
